47e70558359cdaea5c659c31fb7abee6915e6465
James Steinmetz <robotsdanceincolor@gmail.com>
2014-06-11 14:13:45 -0700

    centered waves

--- js/waves.js
+++ js/waves.js
@@ -3,7 +3,7 @@ Math.randomInt = function(min, max) {
 }
 
 Math.randomFloat = function(min, max) {
-    return Math.random() * (max - min) + min;
+    return (Math.random() * (max - min)) + min;
 }
 
 var Wave = function() {
@@ -11,20 +11,23 @@ var Wave = function() {
 }
 Wave.prototype = {
     shuffle: function() {
-        this.color = this.randomRgb();
+        this.color = this.randomRgb(128, 255);
 
         this.angle = Math.randomInt(0, 359);
         this.startAngle = Math.randomInt(0, 359);
+        this.velocity = Math.randomFloat(-0.025, 0.025);
 
-        this.speed = Math.randomFloat(-0.01, 0.01);
-        this.velocity = Math.randomFloat(-0.01, 0.01);
+        var height = Math.randomInt(canvas.height * 0.25, canvas.height * 0.75);
+
+        this.waveStart = canvas.height / 2 - height / 2;
+        this.waveEnd = canvas.height / 2 + height / 2;
     },
 
-    randomRgb: function() {
+    randomRgb: function(min, max) {
         return rgb = 'rgb(' 
-            + Math.randomInt(128, 255) + ',' 
-            + Math.randomInt(128, 255) + ',' 
-            + Math.randomInt(128, 255) + ')';
+            + Math.randomInt(min, max) + ',' 
+            + Math.randomInt(min, max) + ',' 
+            + Math.randomInt(min, max) + ')';
     },
 
     map: function(val, low1, high1, low2, high2)
@@ -36,9 +39,9 @@ Wave.prototype = {
         this.angle = this.startAngle;
         this.startAngle += 0.01;
 
-        for (var x = 0; x < canvas.width; x += 2)
+        for (var x = 0; x < canvas.width; x += 1)
         {
-            var y = this.map(Math.sin(this.angle), -1, 1, 0, canvas.height - 1);
+            var y = this.map(Math.sin(this.angle), -1, 1, this.waveStart, this.waveEnd);
 
             ctx.fillStyle = this.color;
             ctx.fillRect(x, y, 1, 1);
